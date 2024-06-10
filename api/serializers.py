a67c70cbe539030739b0bea5658c0016d1ff9815
BORZ070 <shdgit07@gmail.com>
2024-06-10 20:04:36 +0300

    create new api endpoints + add database postgres in docker

--- api/serializers.py
+++ api/serializers.py
@@ -6,4 +6,16 @@ from main.models import IpInfo
 class IpSerializers(serializers.ModelSerializer):
     class Meta:
         model = IpInfo
+        fields = ['id', 'ip', 'city', 'country', 'location', 'postal']
+
+
+class GetIpSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = IpInfo
         fields = ['id', 'city', 'country', 'location', 'postal']
+
+
+class PostIpSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = IpInfo
+        fields = ['ip']
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from api.views import (IpModelViewSet)
+from api.views import (IpModelViewSet, GetIpModelViewSet, PostIpModelViewSet)
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('history', IpModelViewSet)
+router.register('history', IpModelViewSet, basename='history')
+router.register('get-ip', GetIpModelViewSet, basename='get-ip')
+router.register('post-ip', PostIpModelViewSet, basename='post-ip')
 
 
 urlpatterns = [
